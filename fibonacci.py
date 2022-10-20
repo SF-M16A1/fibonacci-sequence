db9d6e38be27f29c6b8ffa820a4b549f77ac6387
Wallis <93327757+SF-M16A1@users.noreply.github.com>
2022-10-20 19:53:27 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,27 @@
+# Fibonacci Sequence
+# Wallis
+
+def fibonacci(x):
+    y = 0
+    result = [0, 1]
+    c = 0
+    while y < x:
+        z = result[c] + result[c + 1]
+        c += 1
+        y += 1
+        result.append(z)
+    return result
+
+
+print('Fibonacci Sequence generator by Wallis')
+while True:
+    try:
+        num = (int(input('Up to what number do you want the sequence to go?: ')))
+        if num <= 0:
+            print('Please input only positive integers')
+            continue
+        fibo = fibonacci(num)
+        print(fibo[:num])
+    except ValueError:
+        print('The program only accepts integers as input')
+        continue
